914f621c6226103016851654624d4a61e5c7599f
John <john.gariepy25@d155.org>
2024-09-26 07:22:22 -0500

    Initial Commit

---
+++ clickerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerGame
+//
+//  Created by JOHN GARIEPY on 9/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
